aab1ac022983e528f8d105b072e5bf69854f7519
Andrew Yushkevich <and.yushkevich@gmail.com>
2020-04-10 09:26:35 +0300

    create daemon

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
+#!/usr/bin/env node
+
 const net = require("net");
+const fs = require("fs").promises;
+
 const server = net.createServer();
 
+function getHost(data) {
+  return data.toString().split("Host: ")[1].split("\r\n")[0];
+}
+
+function getURLFromData(data) {
+  return data.toString().split("\r\n")[0];
+}
+
 server.on("connection", (clientToProxySocket) => {
-  console.log("Client Connected To Proxy");
   // We need only the data once, the starting packet
+  const urls = [];
+  clientToProxySocket.on("data", (data) => urls.push(getURLFromData(data)));
   clientToProxySocket.once("data", (data) => {
-    // If you want to see the packet uncomment below
-    console.log(data.toString());
+    const HTTP_DEFAULT_PORT = 80;
+    const serverAddress = getHost(data);
 
-    let isTLSConnection = data.toString().indexOf("CONNECT") !== -1;
-
-    // By Default port is 80
-    let serverPort = 80;
-    let serverAddress;
-    if (isTLSConnection) {
-      // Port changed if connection is TLS
-      serverPort = data
-        .toString()
-        .split("CONNECT ")[1]
-        .split(" ")[0]
-        .split(":")[1];
-      serverAddress = data
-        .toString()
-        .split("CONNECT ")[1]
-        .split(" ")[0]
-        .split(":")[0];
-    } else {
-      serverAddress = data.toString().split("Host: ")[1].split("\r\n")[0];
-    }
-
-    console.log(serverAddress);
-
-    let proxyToServerSocket = net.createConnection(
+    const proxyToServerSocket = net.createConnection(
       {
         host: serverAddress,
-        port: serverPort,
+        port: HTTP_DEFAULT_PORT,
       },
       () => {
-        console.log("PROXY TO SERVER SET UP");
-        if (isTLSConnection) {
-          clientToProxySocket.write("HTTP/1.1 200 OK\r\n\n");
-        } else {
-          proxyToServerSocket.write(data);
-        }
+        proxyToServerSocket.write(data);
 
         clientToProxySocket.pipe(proxyToServerSocket);
         proxyToServerSocket.pipe(clientToProxySocket);
-
-        proxyToServerSocket.on("error", (err) => {
-          console.log("PROXY TO SERVER ERROR");
-          console.log(err);
-        });
       },
     );
-    clientToProxySocket.on("error", (err) => {
-      console.log("CLIENT TO PROXY ERROR");
-      console.log(err);
+    proxyToServerSocket.on;
+    proxyToServerSocket.on("data", (data) => {
+      const responseStatus = data.toString("ASCII").split("\r\n")[0];
+      fs.appendFile(
+        "/home/qunapi/ksis/lab4/log.txt",
+        `URL: ${urls.pop()}\n${responseStatus}\n`,
+      );
     });
   });
 });
 
-server.on("error", (err) => {
-  console.log("SERVER ERROR");
-  console.log(err);
-  throw err;
-});
-
-server.on("close", () => {
-  console.log("Client Disconnected");
-});
-
-server.listen(8124, () => {
-  console.log("Server runnig at http://localhost:" + 8124);
+server.listen(8080, () => {
+  console.log("Server runnig at http://localhost:" + 8080);
 });
